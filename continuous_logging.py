17b28c4e3c634b94256ad5a1079ad37cc8c39f34
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-06-20 17:07:11 +0200

    Async continuous logging.

--- continuous_logging.py
+++ continuous_logging.py
@@ -1,12 +1,18 @@
+import asyncio
+
 from power_log_manager import PowerLogManager
+import argparse
+
+
+async def main():
+    print(f"Running metering for {args.device_name}")
+    manager = PowerLogManager(args.device_name)
+    await manager.log_data()
+
 
 if __name__ == '__main__':
-    import argparse
-
     parser = argparse.ArgumentParser(description='Run metering with devices.')
     parser.add_argument('--device_name', type=str, help='Device Name to run metering for.', required=False)
     args = parser.parse_args()
 
-    print(f"Running metering for {args.device_name}")
-    manager = PowerLogManager(args.device_name)
-    manager.log_data()
+    asyncio.run(main())
