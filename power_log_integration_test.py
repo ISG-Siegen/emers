196779c07307a7a7dc069ed71d9a338a8c40ff74
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-06-21 13:23:19 +0200

    Changed name of integration example.

--- power_log_integration_test.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from power_log_manager import PowerLogManager
-
-manager = PowerLogManager("mock_plug", experiment_name="mock_plug", polling=0.5, log_interval=3)
-manager.start_experiment_logging()
-print("Sleeping")
-sleep(4)
-print("Done sleeping")
-manager.finish_experiment_logging()
