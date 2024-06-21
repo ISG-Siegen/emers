a4663534ba060d1bffcd8806f6597abf247822e7
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-06-21 13:23:55 +0200

    Added documentation.

--- meters/mock_api.py
+++ meters/mock_api.py
@@ -5,6 +5,11 @@ from power_log_manager import PowerLogResult
 
 
 async def get_data_mock(**kwargs) -> PowerLogResult:
+    """
+    Get data from mock device for debugging purposes
+    :param kwargs: No keyword arguments required
+    :return: PowerLogResult containing energy readings with randomized values
+    """
     current_draw = random.randint(20, 250)
     total_draw = random.randint(1, 5)
     timestamp = time()
