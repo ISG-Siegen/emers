f80b43b2cf8cb0ade55caa2db5fe91bd2b89c434
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-06-21 13:23:44 +0200

    Added documentation and fixed total draw calculation.

--- meters/shelly_api.py
+++ meters/shelly_api.py
@@ -4,6 +4,11 @@ from power_log_manager import PowerLogResult
 
 
 async def get_data_shelly(**kwargs) -> PowerLogResult:
+    """
+    Get data from Shelly Plug Plus S
+    :param kwargs: Must include "device_ip" and "device_id"
+    :return: PowerLogResult containing energy readings
+    """
     headers = {'Content-Type': 'application/x-www-form-urlencoded', }
     request_data = ('{"id":1, '
                     f'"src":"{kwargs["device_id"]}", '
@@ -18,7 +23,7 @@ async def get_data_shelly(**kwargs) -> PowerLogResult:
     electricity_info = data['result']
 
     current_draw = electricity_info['apower']
-    total_draw = electricity_info['aenergy']['total']
+    total_draw = electricity_info['aenergy']['total'] / 1000
     timestamp = time()
 
     return PowerLogResult(timestamp=timestamp, current_draw=current_draw, total_draw=total_draw, misc=None)
