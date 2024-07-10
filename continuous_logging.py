717bb5e51721eaae8228805d8748e454f63b98c1
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-07-10 16:31:49 +0200

    Added arguments for all settings.

--- continuous_logging.py
+++ continuous_logging.py
@@ -5,14 +5,18 @@ import argparse
 
 
 async def main():
-    print(f"Running metering for {args.device_name}")
-    manager = PowerLogManager(args.device_name, experiment_name="continuous", polling=0.5, log_interval=300)
+    print(f"Running continuous logging for {args.device_name} with "
+          f"polling rate {args.polling_rate} and log interval {args.log_interval}...")
+    manager = PowerLogManager(device_name=args.device_name, experiment_name="continuous",
+                              polling_rate=args.polling_rate, log_interval=args.log_interval)
     await manager.log_data()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run metering with devices.')
-    parser.add_argument('--device_name', type=str, help='Device Name to run metering for.', required=False)
+    parser.add_argument('--device_name', type=str, required=True)
+    parser.add_argument('--polling_rate', type=str, required=False, default=0.5)
+    parser.add_argument('--log_interval', type=str, required=False, default=300)
     args = parser.parse_args()
 
     asyncio.run(main())
