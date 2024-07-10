1bac4baed49a7e9ce020761cccdaf0ec62f508c6
Lukas Wegmeth <lukas.wegmeth@uni-siegen.de>
2024-07-10 16:31:11 +0200

    Moved integration example into readme.

--- integration_example.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-from power_log_manager import PowerLogManager
-
-with PowerLogManager("mock_plug", experiment_name="test_experiment", polling=0.5, log_interval=3) as manager:
-    sleep(10)
